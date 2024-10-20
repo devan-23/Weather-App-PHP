7437c6e75e9bc2e1222cd0b4f574e70ca33c96ef
Deva narayanan P.K. <107259499+devan-23@users.noreply.github.com>
2024-10-20 23:15:14 +0530

    Update get_head.php

--- get_head.php
+++ get_head.php
@@ -1,1 +1,13 @@
+<?php
+header('Content-Type: application/json');
 
+// Read tags from JSON file
+function getHead() {
+    $jsonContent = file_get_contents('data/head.json');
+    $data = json_decode($jsonContent, true);
+    return $data['head'];
+}
+
+$head = getHead();
+echo json_encode($head);
+?>
